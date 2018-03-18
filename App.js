dc700bafb76dc390949d8ed05afb874aa5a5f517
ntpbright <nuttapatprom.c@ku.com>
2018-03-19 02:37:41 +0700

    change backgroud to black

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const DATA = {
+  timer: 1234567,
+  labs: [12345, 2345, 34567, 98765],
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0D0D0D',
+    alignItems: 'center',
+  },
+})
